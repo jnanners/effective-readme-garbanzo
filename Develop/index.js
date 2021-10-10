99eb1757e6f9b6b515cc317728c3409de05bab4a
JBanana <johnandrewharris3@gmail.com>
2021-10-10 14:06:55 -0500

    questions and generateMarkdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,111 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const {renderLicenseBadge, renderLicenseLink, renderLicenseSection, generateMarkdown} = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project? (Required)",
+        validate: titleInput => {
+            if(titleInput){
+                return true;
+            }
+            else{
+                console.log("Please enter your project title!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please give a detailed description of your project. (Required)",
+        validate: decriptionInput => {
+            if(decriptionInput){
+                return true;
+            }
+            else{
+                console.log("Please enter a description!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please give intructions for installing your project. (Required)",
+        validate: installationInput => {
+            if(installationInput){
+                return true;
+            }
+            else{
+                console.log("Please enter installation instructions!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please give intructions and examples for using your project. (Required)",
+        validate: usageInput => {
+            if(usageInput){
+                return true;
+            }
+            else{
+                console.log("Please enter usage instructions!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select an open source license for your project. (Required)",
+        choices: ["Unlicensed", "Boost Software", "MIT", "Apache", "Mozilla Public", "GNU LGPLv3", "GNU GPLv3", "GNU AGPLv3"]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Provice guidelines for how other developers can contribute to your project. (Optional)"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Write tests for your application and provide examples on how to run them. (Optional)"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Enter your Github username for the Questions section. (Required)",
+        validate: usernameInput => {
+            if(usernameInput){
+                return true;
+            }
+            else{
+                console.log("Please input your Github username!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email address for the questions section. (Optional)"
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    return inquirer.prompt(questions)
+}
 
 // Function call to initialize app
-init();
+init()
+.then(readmeData => {
+    console.log(readmeData);
+})
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,8 +13,28 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ## Description
+    ${data.description}
+  ## Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Questions](#questions)
+  ## <a name='installation'>Installation</a>
+    ${data.installation}
+  ## <a name='usage'>Usage</a>
+    ${data.usage}
+  ## <a name='license'>License</a>
+    ${data.license}
+  ## Contributing
+    ${data.contributing}
+  ## Tests
+    ${data.tests}
+  ## <a name='questions'>Questions</a>
+    If you have questions you can:
+      - Contact me on Github at [github.com/${data.username}](https://github.com/${data.username})
+      - Email me at ${data.email}
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {renderLicenseBadge, renderLicenseLink, renderLicenseSection, generateMarkdown};
